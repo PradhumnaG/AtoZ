5dafdde30d21772090a6f28c90b372ea5b9c265c
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-06 00:08:35 +0530

    day36

---
+++ src/slidingwindow/BinarySubarraysWithSum.java
@@ -0,0 +1,24 @@
+package slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinarySubarraysWithSum  {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        int sum = 0;
+        for(int num : nums){
+            sum+=num;
+            if(sum == goal){
+                count++;
+            }
+            if(map.containsKey(sum -goal)){
+                count += map.get(sum -goal);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
+
