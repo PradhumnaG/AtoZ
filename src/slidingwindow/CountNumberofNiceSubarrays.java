afd23d5cdd0202f3db13e760d71dc6771347a92f
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-06 17:32:09 +0530

    day36

--- src/slidingwindow/CountNumberofNiceSubarrays.java
+++ src/slidingwindow/CountNumberofNiceSubarrays.java
@@ -5,14 +5,16 @@ import java.util.Map;
 
 public class CountNumberofNiceSubarrays {
     public int numberOfSubarrays(int[] nums, int k) {
-        int res = 0,cur = 0;
-        Map<Integer,Integer> map = new HashMap();
-        map.put(0,1);
-        for(int num :nums){
+        int res = 0, cur = 0;
+        Map<Integer, Integer> map = new HashMap();
+        map.put(0, 1);
+        for (int num : nums) {
             cur += num % 2;
-            res += map.getOrDefault(cur -k,0);
-            map.put(cur,map.getOrDefault(cur,0)+1);
+            res += map.getOrDefault(cur - k, 0);
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
         }
         return res;
 
     }
+
+}
