9864ace209f5e59824e4eae983908a4916c91871
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-06 14:50:57 +0530

    day36

--- src/slidingwindow/CountNumberofNiceSubarrays.java
+++ src/slidingwindow/CountNumberofNiceSubarrays.java
@@ -1,4 +1,18 @@
 package slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountNumberofNiceSubarrays {
-}
+    public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0,cur = 0;
+        Map<Integer,Integer> map = new HashMap();
+        map.put(0,1);
+        for(int num :nums){
+            cur += num % 2;
+            res += map.getOrDefault(cur -k,0);
+            map.put(cur,map.getOrDefault(cur,0)+1);
+        }
+        return res;
+
+    }
