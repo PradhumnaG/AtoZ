f1d5b7a390c6e20d5f8043075c53ac0a3f8ea7db
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-08 23:35:45 +0530

    day38

---
+++ src/slidingwindow/NumberofSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,31 @@
+package slidingwindow;
+
+
+
+ class NumberofSubstringsContainingAllThreeCharacters {
+        public int numberOfSubstrings(String s) {
+            int[] count = new int[3]; // Frequency array for a, b, c
+            int left = 0;
+            int result = 0;
+            int n = s.length();
+
+            for (int right = 0; right < n; right++) {
+                // Add current character to window
+                count[s.charAt(right) - 'a']++;
+
+                // While window is valid (has at least one of a, b, and c)
+                while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                    // Remove character from the left to shrink window
+                    count[s.charAt(left) - 'a']--;
+                    left++;
+                }
+
+                // 'left' is now the number of valid substrings ending at 'right'
+                // logic: indices 0, 1, ... left-1 are all valid starting points
+                result += left;
+            }
+
+            return result;
+        }
+    }
+
