cc3b3847cc10c191b297db3d6f222df1ac1f314b
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-28 11:11:07 +0530

    day57

---
+++ src/dinamicprogramming/SellStocksPartTwo.java
@@ -0,0 +1,19 @@
+package dinamicprogramming;
+
+public class SellStocksPartTwo  {
+    public int maxProfit(int[] prices) {
+        int bd = 0,sd = 0, p = 0;
+        for(int i = 1;i < prices.length;i++){
+            if(prices[i] >= prices[i-1]){
+                sd++;
+            }
+            else{
+                p += prices[sd] - prices[bd];
+                bd = sd = i;
+            }
+        }
+        p += prices[sd] - prices[bd];
+        return p;
+
+    }
+}
