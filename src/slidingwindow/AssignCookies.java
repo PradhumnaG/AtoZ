318e1285609b9a6eb2bfea17f8078d4066a0efd5
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-03-03 22:57:56 +0530

    day60

--- src/slidingwindow/AssignCookies.java
+++ src/slidingwindow/AssignCookies.java
@@ -1,4 +1,29 @@
 package slidingwindow;
 
-public class AssignCookies {
+ import java.util.Arrays;
+
+class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        // Sort the greed factors and cookie sizes
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int childIndex = 0;
+        int cookieIndex = 0;
+
+        // Traverse through both arrays
+        while (childIndex < g.length && cookieIndex < s.length) {
+            // If the current cookie is big enough for the current child
+            if (s[cookieIndex] >= g[childIndex]) {
+                // The child is content, move to the next child
+                childIndex++;
+            }
+            // Regardless of whether the cookie was used or too small,
+            // we move to the next cookie.
+            cookieIndex++;
+        }
+
+        // The childIndex will represent the exact number of children we managed to make content
+        return childIndex;
+    }
 }
