b6a12a4544b423c54b92212f6df0d4a3819be789
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-03-23 23:50:44 +0530

    day67

---
+++ src/string/SortCharacterByFrequency.java
@@ -0,0 +1,42 @@
+package string;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SortCharacterByFrequency  {
+    public String frequencySort(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+
+        List<Character>[] buckets = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int freq = map.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (buckets[i] != null) {
+                for (char c : buckets[i]) {
+
+                    for (int j = 0; j < i; j++) {
+                        sb.append(c);
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
